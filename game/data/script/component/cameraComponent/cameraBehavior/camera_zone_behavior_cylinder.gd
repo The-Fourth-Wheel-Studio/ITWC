a71481bfbe510a99304bb744e1e11682e8a08f18
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-03 13:12:36 +0100

    backend + i^2 = -1

---
+++ game/data/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_cylinder.gd
@@ -0,0 +1,15 @@
+@icon("res://editor/assets/component/cameraZoneBehaviorComponent.png")
+extends cameraZoneBehavior
+class_name cameraZoneBehaviorCylinder
+
+@export var radius : float = 1.0
+@export var yOffset : float = 1.0
+@export var height : float = 1.0
+
+var teta : float = 0.0
+
+func behave():
+	teta = (Common.getComplexArgument(GameManager.player.position.x - self.position.x, GameManager.player.position.z - self.position.z))
+	GameManager.currentCamera.position = self.position + Common.convertCylindricalToCartisia(radius,teta,GameManager.player.position.y + yOffset)
+	GameManager.currentCamera.look_at(GameManager.player.position)
+	GameManager.currentCamera.getNewReference()
