6c25568332441aeeb7ec4cf4466ab82c6960bcc0
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-09 14:59:20 +0100

    clean up and i definitly have to take a shower but i dont have hot water yet T-T

--- game/data/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_planar.gd
+++ game/data/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_planar.gd
@@ -1,6 +1,15 @@
 @icon("res://editor/assets/component/cameraZoneBehaviorComponent.png")
+@tool
 extends cameraZoneBehavior
 class_name cameraZoneBehaviorPlanar
+
+@export var x : Vector3
+@export var y : Vector3
+	
+var plan : Plan
+
+func _ready() -> void:
+	plan = Plan.new(x,y,self.transform + self.get_parent().transform)
 	
 func behave():
 	
