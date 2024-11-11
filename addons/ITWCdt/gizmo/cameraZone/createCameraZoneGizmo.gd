b83fa6db9ab966e41cfc49661559e34df6969000
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-11 17:17:17 +0100

    remove useless gizmo, yes i'm making a meca (i cant' solve so i delete)

--- addons/ITWCdt/gizmo/cameraZone/createCameraZoneGizmo.gd
+++ addons/ITWCdt/gizmo/cameraZone/createCameraZoneGizmo.gd
@@ -2,21 +2,17 @@
 extends EditorNode3DGizmoPlugin
 
 const cameraZoneColliderGizmo = preload("res://addons/ITWCdt/gizmo/cameraZone/gizmo/cameraZoneColliderGizmo.gd")
-const cameraZoneBehaviorStartPosGizmo = preload("res://addons/ITWCdt/gizmo/cameraZone/gizmo/cameraZoneBehaviorStartPosGizmo.gd")
-
 const cameraZoneColliderType = preload("res://game/data/script/component/camera_zone_collider.gd")
-const cameraZoneBehaviorStartPosType = preload("res://game/data/script/component/cameraComponent/point/camera_zone_behavior_start_pos_component.gd")
 
 
 func _init():
 	pass
 func _get_gizmo_name():
-	return "ITWC Gizmos - CameraZone"
+	return "- ITWC Gizmos - CameraZoneCOllider"
 
 func _create_gizmo(node):
 	if node is cameraZoneColliderType:
 		return cameraZoneColliderGizmo.new(node)
-	elif node is cameraZoneBehaviorStartPosType:
-		return cameraZoneBehaviorStartPosGizmo.new(node)
+
 	else:
 		return null
