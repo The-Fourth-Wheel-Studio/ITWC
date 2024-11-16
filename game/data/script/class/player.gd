f6a4089e7df8de47819c5016e5a8009fcf135f1a
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-16 18:39:31 +0100

    Revert "i hate radian, BRO USE EULER ANGLE IF THE GOAT IMSELF CREATED IT USE IT !"
    This reverts commit 7a17a98426d8c6701f866fb42cb3d197fe9642ff.

--- game/data/script/class/player.gd
+++ game/data/script/class/player.gd
@@ -1,18 +1,17 @@
 extends CharacterBody3D
 class_name Player
 
-@export_category("Component")
+#var for movement
+var speed : float = 0.0
+@export var playerAttributeVar : playerAttribute
+@export var isAbleToMove : bool = true
+
 @export var velocityHandler : velocityComponent
 @export var inputhandler : inputHandler
 @export var isOnFloor : isOneFloorComponent
 @export var repulseHandler : repulseHandler
 @export var camera : theBestCameraEver
 @export var abilitieManager : abilitiesManager
-@export_category("player Attributes")
-@export var model : MeshInstance3D
-@export var playerAttributeVar : playerAttribute
-@export_category("player Variables")
-@export var isAbleToMove : bool = true
 
 func execute(delta):
 	pass
--- game/data/script/entity/player/abilities/walk.gd
+++ game/data/script/entity/player/abilities/walk.gd
@@ -8,7 +8,5 @@ func doWhatever(delta):
 		speed = lerp(speed, 
 					player.speedVariation(),
 					player.playerAttributeVar.SPEED_VARIATION)
-		var input_dir : Vector2 = player.inputhandler.direction
+		var input_dir = player.inputhandler.direction
 		player.velocityHandler.setVelocityXZ(player.camera.getNewDir(input_dir) * speed)
-		var refVector : Vector2 = GameManager.currentCamera.getNewDir(Vector2(1.0,0.0))
-		player.model.rotation = Vector3(0.0,Vector3(refVector.x,0.0,refVector.y).angle_to(Vector3(player.camera.getNewDir(input_dir).x,0.0,player.camera.getNewDir(input_dir).y)),0.0)
