d5dd6114cba8c91966c6158d8248d5d653385b40
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-16 18:44:24 +0100

    Revert "plane gizmo, yes i have an exam tomorrow"
    This reverts commit 72b79e5696450384f093b640d9aadb378ff74d93.

--- addons/ITWCdt/ITWCdt.gd
+++ addons/ITWCdt/ITWCdt.gd
@@ -1,14 +1,11 @@
 @tool
 extends EditorPlugin
 
-var cameraZoneColliderGizmos = preload("res://addons/ITWCdt/gizmo/cameraZone/createCameraZoneColliderGizmo.gd").new()
-var cameraZoneBehaviorGizmos = preload("res://addons/ITWCdt/gizmo/cameraZone/createCameraZoneBehaviorGizmo.gd").new()
+var TrackGizmos = preload("res://addons/ITWCdt/gizmo/cameraZone/createCameraZoneGizmo.gd").new()
 
 
 func _enter_tree():
-	add_node_3d_gizmo_plugin(cameraZoneColliderGizmos)
-	add_node_3d_gizmo_plugin(cameraZoneBehaviorGizmos)
+	add_node_3d_gizmo_plugin(TrackGizmos)
 
 func _exit_tree():
-	remove_node_3d_gizmo_plugin(cameraZoneColliderGizmos)
-	remove_node_3d_gizmo_plugin(cameraZoneBehaviorGizmos)
+	remove_node_3d_gizmo_plugin(TrackGizmos)
