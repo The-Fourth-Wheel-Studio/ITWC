a8c00a06ca79e46a7e46ddaabc786a6fce552e06
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-16 18:57:38 +0100

    Revert "FIX ! FINALLY ! now time to watch doctor who S5EP8 instead of working !"
    This reverts commit 707b10a897c327ef55565da6f22b469eda65d85f.

--- game/data/script/entity/player/abilities/walk.gd
+++ game/data/script/entity/player/abilities/walk.gd
@@ -3,10 +3,13 @@ extends abilities
 var speed : float = 0.0
 
 func doWhatever(delta):
+	
 	if player.isAbleToMove:
 		
 		speed = lerp(speed, 
 					player.speedVariation(),
 					player.playerAttributeVar.SPEED_VARIATION)
 		var input_dir = player.inputhandler.direction
-		player.velocityHandler.setVelocityXZ(player.camera.getNewDir(input_dir) * speed)
+		
+		player.velocityHandler.setVelocityXZ(player.camera.getNewDir(input_dir) * player.speed)
+		print(player.velocityHandler.getFinalVelocity())
