a52d49de3cceeb11026c6ee021c0f6cf8fcbb489
Titruc <86261407+Titruc@users.noreply.github.com>
2024-11-16 18:36:08 +0100

    Revert "lomax, i've done it (nearly)"
    This reverts commit 96b95b4e04e4a046c947b125bbc73f36952ecdc8.

--- game/data/script/entity/player/abilities/walk.gd
+++ game/data/script/entity/player/abilities/walk.gd
@@ -8,11 +8,7 @@ func doWhatever(delta):
 		speed = lerp(speed, 
 					player.speedVariation(),
 					player.playerAttributeVar.SPEED_VARIATION)
-		player.velocityHandler.setVelocityXZ(player.camera.getNewDir(player.inputhandler.direction) * speed)
-		applyRotation()
-
-func applyRotation():
-	var refVector : Vector2 = GameManager.currentCamera.getNewDir(player.inputhandler.direction)
-	player.model.rotation.y = lerp_angle(player.model.rotation.y,Common.getComplexArgument(refVector.x, refVector.y * -1),0.1)
-
-	
+		var input_dir : Vector2 = player.inputhandler.direction
+		player.velocityHandler.setVelocityXZ(player.camera.getNewDir(input_dir) * speed)
+		var refVector : Vector2 = GameManager.currentCamera.getNewDir(Vector2(1.0,0.0))
+		player.model.rotation.y = lerp_angle(player.model.rotation.y,-1 * Common.getComplexArgument(input_dir.x,input_dir.y),0.1)
