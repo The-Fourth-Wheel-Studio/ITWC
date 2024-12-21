3dd63452091a7d31fd6d074811b526c2fa33ad87
Titruc <86261407+Titruc@users.noreply.github.com>
2024-12-21 11:41:00 +0100

    and after all those -1 commit i can really work on big thing

--- game/data/script/component/cameraComponent/rotationTransition/camera_zone_rotation_transition_fix_angle.gd
+++ game/data/script/component/cameraComponent/rotationTransition/camera_zone_rotation_transition_fix_angle.gd
@@ -2,7 +2,6 @@ extends cameraZoneRotationTransition
 class_name cameraZoneRotationTransitionConstant
 
 func doSomething(speed : float = 1.0):
-	print(GameManager.currentCamera.rotation - cameraZoneVar.reference.rot)
 	if abs(GameManager.currentCamera.rotation - cameraZoneVar.reference.rot) > Vector3(speed + 0.02,speed + 0.02,speed + 0.02):
 		GameManager.currentCamera.rotation = Common.lerpRotation(GameManager.currentCamera.rotation, cameraZoneVar.reference.rot, speed * GameManager.invDelta)
 		return false
