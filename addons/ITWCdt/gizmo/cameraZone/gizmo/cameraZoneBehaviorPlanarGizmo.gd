060d7f53f5e75d724b123cc9a48f2557da3c7d4b
Titruc <86261407+Titruc@users.noreply.github.com>
2025-01-23 20:16:39 +0100

    drawing on screen like a children

--- addons/ITWCdt/gizmo/cameraZone/gizmo/cameraZoneBehaviorPlanarGizmo.gd
+++ addons/ITWCdt/gizmo/cameraZone/gizmo/cameraZoneBehaviorPlanarGizmo.gd
@@ -4,6 +4,7 @@ extends EditorNode3DGizmo
 const cameraZoneBehaviorPlanarComponent = preload("res://editor/component/cameraComponent/cameraZoneBehavior/camera_zone_behavior_planar.tscn")
 
 var node:cameraZoneBehaviorPlanar = null
+var mat : StandardMaterial3D = StandardMaterial3D.new()
 var redMat : StandardMaterial3D = StandardMaterial3D.new()
 var greenMat : StandardMaterial3D = StandardMaterial3D.new()
 
@@ -14,16 +15,10 @@ func _init(node:cameraZoneBehaviorPlanar):
 	redMat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	greenMat.albedo_color = Color(0.0,1.0,0.0,1.0)
 	greenMat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	mat.albedo_color = Color(0.2,0.2,0.2,0.5)
+	mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	
 func _redraw():
 	clear()
-	add_lines(PackedVector3Array([Vector3(0.0,0.0,0.0),Vector3(self.node.point.position.x,self.node.point.position.y,0)]),redMat)
-	add_lines(PackedVector3Array([Vector3(0,0,self.node.point.position.z),Vector3(self.node.point.position.x,self.node.point.position.y,self.node.point.position.z)]),redMat)
-	add_lines(PackedVector3Array([Vector3(0.0,0.0,0.0),Vector3(0,0,self.node.point.position.z)]),greenMat)
-	add_lines(PackedVector3Array([Vector3(self.node.point.position.x,self.node.point.position.y,0),Vector3(self.node.point.position.x,self.node.point.position.y,self.node.point.position.z)]),greenMat)
-	
-	#var boxmesh : BoxMesh = BoxMesh.new()
-	#boxmesh.size = self.node.shape.size
-	#var surface : SurfaceTool = SurfaceTool.new()
-	#surface.create_from(boxmesh,0)
-	#add_mesh(surface.commit(),mat)
+	add_lines(PackedVector3Array([Vector3(0.0,0.0,0.0),self.node.point1.position]),redMat)
+	add_lines(PackedVector3Array([Vector3(0.0,0.0,0.0),self.node.point2.position]),greenMat)
