9acbec304819d217fa1066bfd8e8507f15f671b1
Titruc <86261407+Titruc@users.noreply.github.com>
2025-01-23 01:44:23 +0100

    fuck matter architecture my only love is martin

--- game/data/script/class/Plan.gd
+++ game/data/script/class/Plan.gd
@@ -17,35 +17,29 @@ func _init(x : Vector3, y : Vector3, a : Vector3):
 	self.parametricEquation = [self.normal.x,self.normal.y,self.normal.z, d]
 
 func getOrthogonalProjection(point: Vector3) -> Vector3:
-	# Calculer lambda à l'aide de la fonction getLambda
 	var lambda = getLambda(point)
-	
-	# Récupérer les coefficients de l'équation cartésienne du plan
+
 	var a = self.parametricEquation[0]
 	var b = self.parametricEquation[1]
 	var c = self.parametricEquation[2]
 	
-	# Calculer le point projeté
-	var projected_x = point.x + lambda * a
-	var projected_y = point.y + lambda * b
-	var projected_z = point.z + lambda * c
-	
-	# Retourner le point projeté
+	var projected_x = point.x - lambda * a
+	var projected_y = point.y - lambda * b
+	var projected_z = point.z - lambda * c
 	return Vector3(projected_x, -projected_y, projected_z)
 
 func getLambda(point: Vector3) -> float:
-	# Récupérer les coefficients de l'équation cartésienne du plan
+
 	var a = self.parametricEquation[0]
 	var b = self.parametricEquation[1]
 	var c = self.parametricEquation[2]
 	var d = self.parametricEquation[3]
 	
-	# Calculer la valeur de lambda
-	var numerator = -(a * point.x + b * point.y + c * point.z + d)
+
+	var numerator = (a * point.x + b * point.y + c * point.z + d)
 	var denominator = a * a + b * b + c * c
-	
+	#divide by 0
 	if abs(denominator) < 1e-6:
-		# Si le dénominateur est proche de 0, éviter la division par zéro
 		return 0.0
 	
 	return numerator / denominator
