e038304f4257c6f1d4f255f36ad414449820929c
Titruc <86261407+Titruc@users.noreply.github.com>
2025-01-23 20:38:00 +0100

    craft some rail we need to slide some camera !

---
+++ game/data/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_travelling.gd
@@ -0,0 +1,32 @@
+@icon("res://editor/assets/component/cameraZoneBehaviorComponent.png")
+@tool
+extends cameraZoneBehavior
+class_name cameraZoneBehaviorTravelling
+
+@export var point : cameraZoneBehaviorPlanarPoint
+
+
+@export var x : Vector3
+@export var y : Vector3
+	
+var line : Vector3
+
+func _ready() -> void:
+	line = point.global_position - self.global_position
+
+func behave():
+	setCameraPosition(getCameraPosition())
+
+func _process(delta):
+	setXY()
+
+func setXY():
+	line = point.global_position - self.global_position
+
+func getCameraPosition():
+	return project_point_on_line(GameManager.player.global_position, self.global_position, self.line)
+
+func project_point_on_line(point: Vector3, line_point: Vector3, line_direction: Vector3) -> Vector3:
+	var w = point - line_point
+	var projection = w.dot(line_direction) / line_direction.dot(line_direction) * line_direction
+	return line_point + projection
