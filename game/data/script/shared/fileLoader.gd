41c22804d06c087b87115df141177b6be485cd4e
Titruc <86261407+Titruc@users.noreply.github.com>
2025-01-26 15:20:23 +0100

    it was better when it was math

--- game/data/script/shared/fileLoader.gd
+++ game/data/script/shared/fileLoader.gd
@@ -1,1 +1,14 @@
 class_name FileLoader
+
+static func loadFileToRead(filePath : String):
+	return FileLoader.openFile(filePath, FileAccess.READ)
+
+static func loadFileToWrite(filePath):
+	return FileLoader.openFile(filePath, FileAccess.WRITE)
+
+static func openFile(filePath : String, flag : FileAccess.ModeFlags):
+	if FileAccess.file_exists(filePath):
+		return FileAccess.open(filePath,flag)
+	else:
+		MKUtil.print("Error : the file '" + str(filePath) + "' doesn't exist")
+		return null
