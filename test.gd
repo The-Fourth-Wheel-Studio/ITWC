ed844e92b7b59b9664a70ab192843c52738f9248
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-02 16:58:08 +0100

    Revert "WHAT THE FUCK IS A KILOMETER"
    This reverts commit 2a74a0950229eef238d6db3251c8752f2e95de62.

--- test.gd
+++ test.gd
@@ -3,24 +3,10 @@ extends Node3D
 @export var bubble : Control
 
 func _ready() -> void:
-	setVisible()
+	pass
 
 func _process(delta: float) -> void:
-	isActive(delta)
-
-func isActive(delta : float):
 	bubble.position = get2DPos(GameManager.currentCamera)
-	bubble.size = Vector2(1000,1000) * 1/getDistanceFromCam(GameManager.currentCamera)
-	print(getDistanceFromCam(GameManager.currentCamera))
-	
-func setVisible():
-	bubble.visible = true
-
-func setInvisible():
-	bubble.visible = false
 
 func get2DPos(cam : theBestCameraEver):
-	return cam.unproject_position(self.global_position)
-
-func getDistanceFromCam(cam : theBestCameraEver):
-	return (self.global_position - cam.global_position).length()
+	return cam.unproject_position(self.position)
