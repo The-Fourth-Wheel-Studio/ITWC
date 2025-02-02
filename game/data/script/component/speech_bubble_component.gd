140f0257f2843dc77a83f9e056100a0ec915c7c0
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-02 21:00:49 +0100

    * i^2

--- game/data/script/component/speech_bubble_component.gd
+++ game/data/script/component/speech_bubble_component.gd
@@ -1,10 +1,19 @@
+@tool
 extends Control
 class_name speechBubble
 
 @export var textLabel : Label
 @export var timer : Timer
+@export var display : MarginContainer
+@export var base : Control
 
 var text : String = ""
 var letterIndex : int = 0
 
 var letterTime = 0.03
+
+func setPosition():
+	base.position = Vector2(0, -1)
+
+func _process(delta: float) -> void:
+	setPosition()
