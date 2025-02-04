7be359507e953f9b0715d225902c333631aa979e
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-04 20:01:51 +0100

    beep beep run faster pls coyote is here

--- game/data/script/component/is_on_floor_component.gd
+++ game/data/script/component/is_on_floor_component.gd
@@ -2,13 +2,31 @@
 extends Node3D
 class_name isOneFloorComponent
 
+@export_category("don't touch unless you're Titruc")
 @export var NodeToCheck : Node
+
+@export_category("floor")
 @export var lenght : float
-@onready var raycast : RayCast3D = $floorDetection
+@export var raycast : RayCast3D
+
+@export_category("Coyote Time")
+@export var coyoteTimer : Timer
+@export var coyoteTime : float = 0.01
+
+var wasOnFloor : bool = true
 
 func _ready():
 	raycast.target_position.y = lenght * -1.0
 	
 func isOnFloorImprove():
-	return NodeToCheck.is_on_floor() or raycast.is_colliding()
+	var ok : bool = NodeToCheck.is_on_floor() or raycast.is_colliding()
+	if not ok and wasOnFloor:
+		coyoteTimer.start(coyoteTime)
+	wasOnFloor = ok
+	return ok
 	
+func isCoyoteTime():
+	return not coyoteTimer.is_stopped()
+
+func resetCoyote():
+	coyoteTimer.stop()
--- game/data/script/entity/player/abilities/jump.gd
+++ game/data/script/entity/player/abilities/jump.gd
@@ -5,5 +5,6 @@ func doWhatever(delta):
 	if player.isAbleToMove:
 			# Handle Jump.
 			if player.inputhandler.asJump :
-				if player.isOnFloor.isOnFloorImprove():
+				if player.isOnFloor.isOnFloorImprove() or player.isOnFloor.isCoyoteTime():
+					player.isOnFloor.resetCoyote()
 					player.velocityHandler.setVelocityY(player.playerAttributeVar.JUMP_VELOCITY)
