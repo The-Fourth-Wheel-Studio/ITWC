58d461ae4e26939fae6297dd812681d666817bcf
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-16 21:44:27 +0100

    shitty day sitty life

--- addons/ITWCdt/ITWCdt.gd
+++ addons/ITWCdt/ITWCdt.gd
@@ -8,19 +8,27 @@ var cameraRotationGizmos = preload("res://addons/ITWCdt/gizmo/cameraZone/createC
 var cameraReferenceGizmos = preload("res://addons/ITWCdt/gizmo/cameraZone/createCameraReferenceGizmo.gd").new()
 var pnjDialogueManagerGizmo = preload("res://addons/ITWCdt/gizmo/pnj/createPnjDialogueManagerGizmo.gd").new()
 
+var dataLoaderManagerControl = preload("res://addons/ITWCdt/editorControl/dataLoaderManager.tscn").instantiate()
+
 func _enter_tree():
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, dataLoaderManagerControl)
+	addGizmo()
+	
+func _exit_tree():
+	remove_control_from_docks(dataLoaderManagerControl)
+	removeGizmo()
+	
+func addGizmo():
 	#gizmo
 	add_node_3d_gizmo_plugin(cameraZoneColliderGizmos)
 	add_node_3d_gizmo_plugin(cameraZoneBehaviorGizmos)
 	add_node_3d_gizmo_plugin(cameraRotationGizmos)
 	add_node_3d_gizmo_plugin(cameraReferenceGizmos)
 	add_node_3d_gizmo_plugin(pnjDialogueManagerGizmo)
-	
-func _exit_tree():
-	#gizmo
+func removeGizmo():
+		#gizmo
 	remove_node_3d_gizmo_plugin(cameraZoneColliderGizmos)
 	remove_node_3d_gizmo_plugin(cameraZoneBehaviorGizmos)
 	remove_node_3d_gizmo_plugin(cameraRotationGizmos)
 	remove_node_3d_gizmo_plugin(cameraReferenceGizmos)
 	remove_node_3d_gizmo_plugin(pnjDialogueManagerGizmo)
-	
