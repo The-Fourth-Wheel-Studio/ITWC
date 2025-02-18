7fb5796e430e2bcb45d3178bd465f827b46feac1
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-18 22:49:44 +0100

    Perrine want me sorry

--- core/shared/script/singleton/gameLoader.gd
+++ core/shared/script/singleton/gameLoader.gd
@@ -1,5 +1,10 @@
 static func getGameFile():
-	pass
-
+	var data : ITWCdata = ITWCdata.new()
+	data.load("res://core/ITWC/pack.ITWCdata")
+	var arr : Array
+	for i in data.get_section_keys("game_config"):
+		arr.append(data.get_value("game_config", i))
+	return arr
+	
 static func getModFile():
 	pass
