07664f36065980e8e2ecf0391f659c03e64eb176
Titruc <86261407+Titruc@users.noreply.github.com>
2025-02-26 18:15:01 +0100

    and more spagetti

--- addons/ITWCdt/editorControl/data_loader_manager.gd
+++ addons/ITWCdt/editorControl/data_loader_manager.gd
@@ -25,6 +25,12 @@ static var filePath : String = "core/ITWC"
 @export var menueKey : TextEdit
 @export var menueValue : TextEdit
 
+#game
+@export var gameLoadContainer : VBoxContainer
+@export var gameKey : TextEdit
+@export var gameValue : TextEdit
+
+
 var file : ITWCdata
 
 static func init(txt : String, path : String):
@@ -53,7 +59,15 @@ func _on_menue_button_pressed() -> void:
 		setFileValue("Menue",txt0,txt1)
 	else:
 		MKUtil.print("enable to set value")
-	
+
+func _on_game_button_pressed() -> void:
+	var txt0 : String = gameKey.text
+	var txt1 : String = gameValue.text
+	if(txt0 != "" and txt1 != ""):
+		setFileValue("Game",txt0,txt1)
+	else:
+		MKUtil.print("enable to set value")
+
 func loadITWCdata():
 	var file = ITWCdata.new()
 	file = FileLoader.loadITWCdata(filePath)
@@ -62,12 +76,16 @@ func loadITWCdata():
 func refresh():
 	EngineTool.removeAllChildren(loadContainer)
 	EngineTool.removeAllChildren(menueLoadContainer)
+	EngineTool.removeAllChildren(gameLoadContainer)
 	if file.has_section("OnLoad"):
 		for i in file.get_section_keys("OnLoad"):
 			loadContainer.add_child(ITWCdt_textEdit.init(i,file.get_value("OnLoad",i)))
 	if file.has_section("Menue"):
 		for i in file.get_section_keys("Menue"):
 			menueLoadContainer.add_child(ITWCdt_textEdit.init(i,file.get_value("Menue",i)))
+	if file.has_section("Game"):
+		for i in file.get_section_keys("Game"):
+			gameLoadContainer.add_child(ITWCdt_textEdit.init(i,file.get_value("Game",i)))
 
 func setFileValue(category : String, key : String, value : String):
 	file.set_value(category,key,value)
