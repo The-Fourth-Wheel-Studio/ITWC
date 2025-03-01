68c721af75c64850325a0633d69fe3cf69df88d4
Titruc <86261407+Titruc@users.noreply.github.com>
2025-03-01 13:30:18 +0100

    setup test scene + lil fix on cylinder

--- core/shared/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_cylinder.gd
+++ core/shared/script/component/cameraComponent/cameraBehavior/camera_zone_behavior_cylinder.gd
@@ -11,8 +11,6 @@ var teta : float = 0.0
 func behave():
 	teta = (Common.getComplexArgument(GameManager.player.position.x - self.position.x, GameManager.player.position.z - self.position.z))
 	setCameraPosition(getCameraPosition())
-	GameManager.currentCamera.look_at(GameManager.player.position)
-	GameManager.currentCamera.getNewReference()
 
 func getCameraPosition():
 	return (self.position + Common.convertCylindricalToCartisia(radius,teta,GameManager.player.position.y + yOffset))
