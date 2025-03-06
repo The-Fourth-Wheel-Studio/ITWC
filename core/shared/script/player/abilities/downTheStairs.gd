483c34f7be635fcd270d4fbc98e545e9c756b72e
Titruc <86261407+Titruc@users.noreply.github.com>
2025-03-06 17:37:20 +0100

    down the stairs but buggy

--- core/shared/script/player/abilities/downTheStairs.gd
+++ core/shared/script/player/abilities/downTheStairs.gd
@@ -10,18 +10,18 @@ func load():
 var _snapped_to_stairs_last_frame = false
 func _snap_down_to_stairs_check():
 	var did_snap = false
-	if player.isOnFloor.wasOnFloor and not player.isOnFloor.isOnFloorImprove() and player.velocity.y <= 0: #and $StairsBelowRayCast3D.is_colliding():
-		print("a")
+	if (player.isOnFloor.wasOnFloor or _snapped_to_stairs_last_frame) and not player.isOnFloor.isOnFloorImprove() and player.velocity.y <= 0: #and $StairsBelowRayCast3D.is_colliding():
+		print(_snapped_to_stairs_last_frame)
 		var body_test_result = PhysicsTestMotionResult3D.new()
 		var params = PhysicsTestMotionParameters3D.new()
-		var max_step_down = -0.5
+		var max_step_down = -1
 		params.from = playerFeet.global_transform
 		params.motion = Vector3(0,max_step_down,0)
 		print(PhysicsServer3D.body_test_motion(player.get_rid(), params, body_test_result))
 		if PhysicsServer3D.body_test_motion(player.get_rid(), params, body_test_result):
 			print(body_test_result.get_travel().y)
 			var translate_y = body_test_result.get_travel().y
-			player.position.y -= translate_y
+			player.position.y += translate_y
 			player.apply_floor_snap()
 			did_snap = true
 	_snapped_to_stairs_last_frame = did_snap
