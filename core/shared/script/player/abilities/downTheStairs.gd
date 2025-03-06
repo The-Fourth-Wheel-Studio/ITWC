7ee5d7fbe4134b9398ad4891d12e012bc887364c
Titruc <86261407+Titruc@users.noreply.github.com>
2025-03-07 00:52:07 +0100

    fix down stair

--- core/shared/script/player/abilities/downTheStairs.gd
+++ core/shared/script/player/abilities/downTheStairs.gd
@@ -1,16 +1,19 @@
 extends abilitiesAfterPhysics
 
 @export var playerFeet : Node3D
+@export var isStair : RayCast3D
 
 var wasSnap = false
 
 func load():
 	self.player = GameManager.player
+	isStair.target_position.y = player.playerAttributeVar.MAX_STAIR_SIZE + player.playerAttributeVar.MAX_STAIR_SIZE/10.0
+	isStair.add_exception(player)
 
 #fall stairs by Majikayo Games (https://www.youtube.com/watch?v=-WjM1uksPIk) 
 func downStair():
 	var ok : bool = false
-	if (player.isOnFloor.wasOnFloor or wasSnap) and not player.isOnFloor.isOnFloorImprove() and player.velocity.y <= 0: #and $StairsBelowRayCast3D.is_colliding():
+	if (player.isOnFloor.wasOnFloor or wasSnap) and not player.isOnFloor.isOnFloorImprove() and player.velocity.y <= 0 and isStair.is_colliding():
 		var bodyTestResult = PhysicsTestMotionResult3D.new()
 		var params = PhysicsTestMotionParameters3D.new()
 		var max_step_down = -0.5
